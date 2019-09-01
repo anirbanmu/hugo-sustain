90e82cbc2879ca6efda30fef65f510508b8bec1c
jigar wala <jigarwala007@gmail.com>
2019-09-01 21:56:37 +0530

    lazy loading like medium :fire: , check examplesite for more info

---
+++ exampleSite/content/blog/medium-like-image-loading/index.md
@@ -0,0 +1,8 @@
+---
+author: "Jigar Wala"
+date: 2019-09-31T16:05:14+05:30
+title: Medium like image loading
+---
+
+
+{{< lazyImage src="profile.png">}}
--- static/js/main.js
+++ static/js/main.js
@@ -9,6 +9,76 @@ jQuery(document).ready(function(e) {
             scrollTop: e(this.hash).offset().top
         }, 1e3)
     })
+
+    /**  lazy image loading  */
+
+    var lazyloadImages;
+
+    if ("IntersectionObserver" in window) {
+        lazyloadImages = document.querySelectorAll(".lazy");
+        var imageObserver = new IntersectionObserver(function (entries, observer) {
+
+            entries.forEach(function (entry) {
+
+
+                let image = entry.target;
+                var smallImg = new Image();
+                smallImg.src = image.src;
+                smallImg.onload = function () {
+                    image.classList.add('loaded');
+                };
+
+                if (entry.isIntersecting) {
+
+                    let ogImage = new Image();
+
+                    ogImage.src = image.dataset.src;
+                    ogImage.onload = function(){
+                        ogImage.classList.add("loaded")
+                    }
+
+                    image.parentNode.appendChild(ogImage)
+                    image.classList.remove("lazy");
+                    imageObserver.unobserve(image);
+                }
+            });
+        });
+
+        lazyloadImages.forEach(function (image) {
+            imageObserver.observe(image);
+        });
+    } else {
+        // fall back to older browsers
+        var lazyloadThrottleTimeout;
+        lazyloadImages = document.querySelectorAll(".lazy");
+
+        function lazyload() {
+            if (lazyloadThrottleTimeout) {
+                clearTimeout(lazyloadThrottleTimeout);
+            }
+
+            lazyloadThrottleTimeout = setTimeout(function () {
+                var scrollTop = window.pageYOffset;
+                lazyloadImages.forEach(function (img) {
+                    if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                        img.src = img.dataset.src;
+                        img.classList.remove('lazy');
+                    }
+                });
+                if (lazyloadImages.length == 0) {
+                    document.removeEventListener("scroll", lazyload);
+                    window.removeEventListener("resize", lazyload);
+                    window.removeEventListener("orientationChange", lazyload);
+                }
+            }, 20);
+        }
+
+        document.addEventListener("scroll", lazyload);
+        window.addEventListener("resize", lazyload);
+        window.addEventListener("orientationChange", lazyload);
+    }
+
 });
+
 
 
